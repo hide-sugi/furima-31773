6e88d41c77740460b5bb7b56c1a979b71d37c9cb
hide-sugi <hsdgot711711@gmail.com>
2020-11-22 17:58:03 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :status
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :preparationday
+  with_options presence: true do
+    validates :name
+    validates :item_explanation
+    validates :item_category_id,       numericality: { other_than: 1 }
+    validates :item_condition_id,      numericality: { other_than: 1 }
+    validates :delivery_fee_status_id, numericality: { other_than: 1 }
+    validates :shipping_prefecture_id, numericality: { other_than: 1 }
+    validates :preparation_day_id,     numericality: { other_than: 1 }
+    validates :price
+    validates :user
+  end
 end
---
+++ spec/models/preparationday_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Preparationday, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
